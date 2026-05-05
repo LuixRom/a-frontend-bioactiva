6714248f4981e2229c6958fe9f8c473909ca7e9c
JosephAnderson234 <joseph.cose@utec.edu.pe>
2026-05-05 12:49:18 -0500

    actualización de la configuración de CORS para permitir todos los orígenes

--- sunat_service/main.py
+++ sunat_service/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Solo el frontend local
+    allow_origins=["*"],  # Solo el frontend local
     allow_credentials=True,
     allow_methods=["GET"],
     allow_headers=["*"],
