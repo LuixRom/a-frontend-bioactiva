2f58d955b286254cb25f3e48d397ddfad7dcef89
AnthRom <luis.romero@utec.edu.pe>
2026-05-05 15:29:40 -0500

    tercera corrección

--- src/lib/msalConfig.ts
+++ src/lib/msalConfig.ts
@@ -8,7 +8,6 @@ export const msalConfig: Configuration = {
   },
   cache: {
     cacheLocation: 'localStorage', // Use localStorage to share sessions across tabs
-    storeAuthStateInCookie: false,
   },
 };
 
